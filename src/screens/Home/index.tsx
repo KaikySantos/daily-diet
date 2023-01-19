6c1b21e673dfe1a0ea86128f8134476b3cfa4539
kaikySantos <info.kaiky@gmail.com>
2023-01-18 21:46:59 -0300

    basic structure

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+import { Text } from "react-native";
+
+export function Home() {
+  return (
+    <Container>
+      <Text>Hello World!</Text>
+    </Container>
+  )
+}
