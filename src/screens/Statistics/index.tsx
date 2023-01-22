f5ce858c9621d5edf64c36e31d2361183e117fac
kaikySantos <info.kaiky@gmail.com>
2023-01-21 23:23:41 -0300

    feat: statistics screen finished

--- src/screens/Statistics/index.tsx
+++ src/screens/Statistics/index.tsx
@@ -2,7 +2,7 @@ import { useTheme } from "styled-components/native";
 
 import { StatisticHeader } from "@components/StatisticHeader";
 
-import { Container } from "./styles";
+import { Card, CardRow, CardText, CartTitle, Container, Content, Title } from "./styles";
 
 export function Statistics() {
   const { COLORS: { GREEN_LIGHT, RED_LIGHT } } = useTheme();
@@ -14,6 +14,36 @@ export function Statistics() {
   return (
     <Container background={containerColor} >
       <StatisticHeader percentage={percentage} />
+
+      <Content>
+        <Title>Estatísticas gerais</Title>
+
+        <CardRow>
+          <Card background="default">
+            <CartTitle>4</CartTitle>
+            <CardText>melhor sequência de pratos dentro da dieta</CardText>
+          </Card>
+        </CardRow>
+
+        <CardRow>
+          <Card background="default">
+            <CartTitle>109</CartTitle>
+            <CardText>refeições registradas</CardText>
+          </Card>
+        </CardRow>
+
+        <CardRow>
+          <Card background="green" style={{ marginRight: 6 }}>
+            <CartTitle>32</CartTitle>
+            <CardText>refeições dentro da dieta</CardText>
+          </Card>
+
+          <Card background="red" style={{ marginLeft: 6 }}>
+            <CartTitle>77</CartTitle>
+            <CardText>refeições fora da dieta</CardText>
+          </Card>
+        </CardRow>
+      </Content>
     </Container>
   )
 }
--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -9,4 +9,74 @@ type Props = {
 export const Container = styled(SafeAreaView)<Props>`
   flex: 1;
   background: ${({ background }) => background};
+`
+
+export const Content = styled.View`
+  background: ${({ theme }) => theme.COLORS.GRAY_7};
+  flex: 1;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+
+  padding: 0px 24px;
+`
+
+export const Title = styled.Text`
+  text-align: center;
+
+  margin-top: 33px;
+  margin-bottom: 23px;
+
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_1};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`
+
+type CardProps = {
+  background: "default" | "green" | "red"; 
+}
+
+export const Card = styled.View<CardProps>`
+  flex: 1;
+  background: ${({ theme }) => theme.COLORS.GRAY_6};
+  border-radius: 8px;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 12px;
+  
+  ${({ theme, background }) => css`
+    ${background === "green" && {
+      background: theme.COLORS.GREEN_LIGHT
+    }}
+
+    ${background === "red" && {
+      background: theme.COLORS.RED_LIGHT
+    }}
+  `}
+`
+
+export const CartTitle = styled.Text`
+  margin-bottom: 8px;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_1};
+    font-size: ${theme.FONT_SIZE.XL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
+`
+
+export const CardText = styled.Text`
+  text-align: center;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_2};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
+`
+
+export const CardRow = styled.View`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
 `
